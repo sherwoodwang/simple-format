78c0f6972b930928cbcac91c562bb4244d62eca9
Sherwood Wang | 王嘯宇 <swang.work@gmail.com>
2014-10-29 14:36:02 +0800

    setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+        name = "readable_text",
+        version = "0.1",
+        packages = find_packages(),
+        )
