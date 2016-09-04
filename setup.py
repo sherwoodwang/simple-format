544246f115b8c8baef086a3126e395037c4b86db
Sherwood Wang <sherwood@wang.onl>
2016-09-04 15:12:56 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(
-        name = "simple_format",
-        version = "0.1",
-        packages = find_packages(),
-        )
+    name="simple_format",
+    version="0.1",
+    packages=find_packages(),
+    description='A simple text formatter converting a readable plain text to html',
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    author='Sherwood Wang',
+    author_email='sherwood@wang.onl',
+    url='https://github.com/sherwoodwang/simple-format',
+)
